ed30133ad1b9faaa93953b5ad0006a9fa158b141
Anis <anissheikh80@gmail.com>
2026-04-06 17:45:44 +0530

    fixed deployed file

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World V3 deploy using github actions and aws ecs');
+  res.send('Hello World V4 deployed');
 });
 
 app.get('/health', (req, res) => {
