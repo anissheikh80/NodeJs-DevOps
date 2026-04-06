75246ad9d6746373f39e4737c100d396291ec857
Anis <anissheikh80@gmail.com>
2026-04-06 16:56:17 +0530

    Added github workflows to deploy on AWS ECS

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello World V3 deploy using github actions and aws ecs');
 });
 
 app.get('/health', (req, res) => {
